4eed775719efc894e32c413a5151a8a9b455dd9f
muratguven <murat.guven@vodafone.com>
2026-05-11 22:48:27 +0300

    eleventy installed. converted index.html to sub modules. deleted react files

--- src/components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Card = ({ children, className = "" }) => (
-  <div className={`bg-white rounded-xl ${className}`}>{children}</div>
-);
-
-export const CardContent = ({ children, className = "" }) => (
-  <div className={className}>{children}</div>
-);
